f5d6187400aa461720c632d31c6f7b669c9dd4ff
Ayush Jaiswal <jaiswalayush131313@gmail.com>
2021-02-09 01:59:01 +0530

    Homepage edited + Added about conference section

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,3 +93,18 @@
   });      
 
 }(jQuery));
+
+function ShowHide() {
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
