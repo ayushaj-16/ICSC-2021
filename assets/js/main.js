2ecbbaf6d5ad20f52050b9d3f2a146898febb16a
Ayush Jaiswal <jaiswalayush131313@gmail.com>
2021-04-13 02:53:41 +0530

    redesign halfway done

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,12 +95,29 @@
 }(jQuery));
 
 function ShowHide() {
+  var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
     btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function ShowHide2() {
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Learn more";
     moreText.style.display = "none";
   } else {
     dots.style.display = "none";
